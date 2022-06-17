a3a0e3aa04fecc216170e3a589173d9b653fb072
Jaspreet Singh Sidhu <sidhujaspreet963@gmail.com>
2022-06-17 13:33:20 +0400

    fix: correct heading tag

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h2 className="box-heading">Box Game</h2>
-      <h4 className="box-description">Click on the boxes below.</h2>
+      <h4 className="box-description">Click on the boxes below.</h4>
       <Game />
     </div>
   );
