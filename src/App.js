4ad63d472f47ca247b9696bee73f9bae65137765
Jaspreet Singh Sidhu <sidhujaspreet963@gmail.com>
2022-06-27 16:56:14 +0400

    feat: remove variants

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import './App.css';
 import Game from './game/game'
 
 function App() {
-  let variant = window.variant || 1;
-  let showDesc = window.variant === 1
   return (
     <div className="App">
       <h2 className="box-heading">Box Game</h2>
-      {showDesc && <h4 className="box-description">Click on the boxes below.</h4>}
+      <h4 className="box-description">Click on the boxes below.</h4>
       <Game />
     </div>
   );
