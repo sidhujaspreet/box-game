bbc0b47259247e46225720dfdf5d5475836c3adb
Jaspreet Singh Sidhu <sidhujaspreet963@gmail.com>
2022-06-27 15:49:01 +0400

    feat: show desc if it is enabled

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Game from './game/game'
 
 function App() {
+  let variant = window.variant || 1;
+  let showDesc = window.variant === 1
   return (
     <div className="App">
       <h2 className="box-heading">Box Game</h2>
-      <h4 className="box-description">Click on the boxes below.</h4>
+      {showDesc && <h4 className="box-description">Click on the boxes below.</h4>}
       <Game />
     </div>
   );
